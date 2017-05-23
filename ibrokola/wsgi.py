d259d6771ebdd056e6802fa0364c883dd535b35c
ibrobabs <babskolawole@gmail.com>
2017-05-23 07:05:45 -0600

    Update

--- ibrokola/wsgi.py
+++ ibrokola/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ibrokola.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ibrokola.settings.production")
 
 application = get_wsgi_application()
